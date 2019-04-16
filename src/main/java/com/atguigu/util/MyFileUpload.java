128e310dd15eba6eab14f8aae3547b9cd44ba5ea
tom_glb <goodMorning_glb@lanfeng.com>
2019-04-16 15:31:43 +0800

    dfasfsd

--- src/main/java/com/atguigu/util/MyFileUpload.java
+++ src/main/java/com/atguigu/util/MyFileUpload.java
@@ -16,7 +16,7 @@ public class MyFileUpload {
 		
 		List<String> list_image = new ArrayList<String>();
 		
-		for (int i = 0; i < files.length; i++) {//dasfsdfg
+		for (int i = 0; i < files.length; i++) {
 			if (!files[i].isEmpty()) {
 				String originalFilename = files[i].getOriginalFilename();
 				String name = System.currentTimeMillis() + originalFilename;
